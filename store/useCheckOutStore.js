62cd150632f28c2e25fdde5bbc13c542f692003f
Shubb3006 <sm325642@gmail.com>
2026-02-06 12:13:04 +0530

    final

--- store/useCheckOutStore.js
+++ store/useCheckOutStore.js
@@ -1,46 +1,52 @@
 import axiosInstance from "@/lib/axios";
+import toast from "react-hot-toast";
 import { create } from "zustand";
-import { toast } from "react-hot-toast";
+import { persist } from "zustand/middleware";
 import { useCartStore } from "./useCartStore";
 
-export const useCheckoutStore = create((set) => ({
-  isCheckingOut: false,
-  selectedAddress:null,
-  orders: [],
+export const useCheckoutStore = create(
+  persist(
+    (set, get) => ({
+      isCheckingOut: false,
+      selectedAddress: null,
+      orders: [],
 
-  checkOut: async ({paymentMethod}) => {
-    const { selectedAddress } = useCheckoutStore.getState();
-    if (!selectedAddress) {
-      toast.error("Please select delivery address");
-      return false;
+      setSelectedAddress: (address) =>
+        set({ selectedAddress: address }),
+
+      clearCheckout: () =>
+        set({ selectedAddress: null }),
+
+      checkOut: async ({paymentMethod}) => {
+        const { selectedAddress } = get();
+        if (!selectedAddress) {
+          toast.error("Please select delivery address");
+          return false;
+        }
+
+        set({ isCheckingOut: true });
+
+        try {
+          const res = await axiosInstance.post("/user/orders", {
+            address: selectedAddress,
+            paymentMethod
+          });
+
+          useCartStore.getState().clearCart();
+          set({ selectedAddress: null }); // IMPORTANT
+          toast.success("Order placed successfully 🎉");
+
+          return true;
+        } catch (e) {
+          toast.error(e?.response?.data?.message || "Checkout failed");
+          return false;
+        } finally {
+          set({ isCheckingOut: false });
+        }
+      },
+    }),
+    {
+      name: "checkout-storage", // localStorage key
     }
-    set({ isCheckingOut: true });
-
-    try {
-      const res = await axiosInstance.post("/user/orders",{ address: selectedAddress,paymentMethod});
-      useCartStore.getState().clearCart();
-      toast.success("Order placed successfully 🎉");
-
-      // optional: store order
-      set((state) => ({
-        orders: [...state.orders, res.data.order],
-      }));
-
-      return true; // useful for redirect
-    } catch (error) {
-      toast.error(
-        error?.response?.data?.message || "Checkout failed"
-      );
-      return false;
-    } finally {
-      set({ isCheckingOut: false });
-    }
-  },
-
-  setSelectedAddress:(address)=>{
-    set({ selectedAddress: address })
-  },
-  clearCheckout: () =>
-    set({ selectedAddress: null }),
-
-}));
+  )
+);
