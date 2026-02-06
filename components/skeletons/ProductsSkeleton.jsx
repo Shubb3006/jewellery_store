a3bf54c1fe3239fbcdd18e4862ccb94d5a3da11b
Shubb3006 <sm325642@gmail.com>
2026-02-06 18:02:23 +0530

    final

--- components/skeletons/ProductsSkeleton.jsx
+++ components/skeletons/ProductsSkeleton.jsx
@@ -1,36 +1,35 @@
 const ProductsSkeleton = () => {
-    return (
-      <section className="p-6 max-w-7xl mx-auto animate-pulse">
-        {/* Title */}
-        <div className="h-7 w-48 bg-base-300 rounded mb-6" />
-  
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {[...Array(8)].map((_, i) => (
-            <div
-              key={i}
-              className="rounded-xl border border-gray-200 shadow-sm overflow-hidden bg-base-100"
-            >
-              {/* Image */}
-              <div className="h-52 bg-base-300" />
-  
-              {/* Content */}
-              <div className="p-4 space-y-3">
-                <div className="h-4 w-full bg-base-300 rounded" />
-                <div className="h-4 w-3/4 bg-base-300 rounded" />
-  
-                <div className="h-6 w-24 bg-base-300 rounded mt-2" />
-              </div>
-  
-              {/* Button */}
-              <div className="px-4 pb-4">
-                <div className="h-10 w-full bg-base-300 rounded-lg" />
-              </div>
+  return (
+    <section className="p-6 max-w-7xl mx-auto animate-pulse">
+      {/* Title */}
+      <div className="h-7 w-48 bg-base-300 rounded mb-6" />
+
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {[...Array(8)].map((_, i) => (
+          <div
+            key={i}
+            className="rounded-xl border border-gray-200 shadow-sm overflow-hidden bg-base-100"
+          >
+            {/* Image */}
+            <div className="h-36 sm:h-44 md:h-52 bg-base-300" />
+
+            {/* Content */}
+            <div className="p-4 space-y-3">
+              <div className="h-2 sm:h-4 w-full bg-base-300 rounded" />
+              <div className="h-2 sm:h-4 w-3/4 bg-base-300 rounded" />
+
+              <div className="h-3 sm:h-6 w-24 bg-base-300 rounded mt-2" />
             </div>
-          ))}
-        </div>
-      </section>
-    );
-  };
-  
-  export default ProductsSkeleton;
-  
+
+            {/* Button */}
+            <div className="px-4 pb-4">
+              <div className="h-10 w-full bg-base-300 rounded-lg" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ProductsSkeleton;
