09bc89de20c2d28c6a4d0f95d94fd4a2cf1bb65b
Shubb3006 <sm325642@gmail.com>
2026-03-04 17:38:47 +0530

    final

--- components/AllProductPage.jsx
+++ components/AllProductPage.jsx
@@ -4,10 +4,11 @@ import { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import useProductStore from "@/store/useProductStore";
 import ProductsPage from "@/components/Product";
+import AllProductsPageSkeleton from "./skeletons/ProductsSkeleton";
 
 export default function ProductSection() {
   const searchParams = useSearchParams();
-  const { getAllProducts, products } = useProductStore();
+  const { getAllProducts, products, gettingAllProducts } = useProductStore();
 
   const activeCategory = searchParams.get("category");
   const activeSort = searchParams.get("sort");
@@ -18,15 +19,17 @@ export default function ProductSection() {
 
   console.log(products);
 
+  if (true) return <AllProductsPageSkeleton />;
+
   return (
     <main className="flex-1 p-8">
       {products.length === 0 ? (
-          <div className="text-center text-gray-500 mt-20">
-            No products available
-          </div>
-        ) : (
-          <ProductsPage products={products} />
-        )}
+        <div className="text-center text-gray-500 mt-20">
+          No products available
+        </div>
+      ) : (
+        <ProductsPage products={products} />
+      )}
     </main>
   );
 }
--- components/skeletons/ProductsSkeleton.jsx
+++ components/skeletons/ProductsSkeleton.jsx
@@ -1,32 +1,36 @@
 const ProductsSkeleton = () => {
   return (
-    <section className="p-6 max-w-7xl mx-auto animate-pulse">
-      {/* Title */}
-      <div className="h-7 w-48 bg-base-300 rounded mb-6" />
+    <section className="p-6 mx-auto animate-pulse">
+      <div className="flex gap-8">
+        {/* ================= PRODUCTS GRID ================= */}
+        <div className="flex-1">
+          {/* Title */}
+          <div className="h-7 w-48 bg-base-300 rounded mb-6" />
 
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {[...Array(8)].map((_, i) => (
-          <div
-            key={i}
-            className="rounded-xl border border-gray-200 shadow-sm overflow-hidden bg-base-100"
-          >
-            {/* Image */}
-            <div className="h-36 sm:h-44 md:h-52 bg-base-300" />
+          <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            {[...Array(8)].map((_, i) => (
+              <div
+                key={i}
+                className="rounded-xl border border-gray-200 shadow-sm overflow-hidden bg-base-100"
+              >
+                {/* Image */}
+                <div className="h-36 sm:h-44 md:h-52 bg-base-300" />
 
-            {/* Content */}
-            <div className="p-4 space-y-3">
-              <div className="h-2 sm:h-4 w-full bg-base-300 rounded" />
-              <div className="h-2 sm:h-4 w-3/4 bg-base-300 rounded" />
+                {/* Content */}
+                <div className="p-4 space-y-3">
+                  <div className="h-2 sm:h-4 w-full bg-base-300 rounded" />
+                  <div className="h-2 sm:h-4 w-3/4 bg-base-300 rounded" />
+                  <div className="h-3 sm:h-6 w-24 bg-base-300 rounded mt-2" />
+                </div>
 
-              <div className="h-3 sm:h-6 w-24 bg-base-300 rounded mt-2" />
-            </div>
-
-            {/* Button */}
-            <div className="px-4 pb-4">
-              <div className="h-10 w-full bg-base-300 rounded-lg" />
-            </div>
+                {/* Button */}
+                <div className="px-4 pb-4">
+                  <div className="h-10 w-full bg-base-300 rounded-lg" />
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </section>
   );
